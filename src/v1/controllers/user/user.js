213fc7830cae611162b38a038f457d4adbc35913
loadlc2003 <banerjeeankush184@gmail.com>
2024-09-26 17:50:11 +0530

    wip

--- src/v1/controllers/user/user.js
+++ src/v1/controllers/user/user.js
@@ -285,7 +285,25 @@ res.status(400).json({
         where: {
           id: req.user.id,
         },
-       
+        include:{
+          
+          tests:{
+          
+            include:{
+              attempts:{
+                include:{
+                  submissions:{
+                    include:{
+                      question:true
+                    }
+                  }
+                }
+              }
+            }
+          },
+          TestAttempt:true
+        
+        },
       
       });
       res.json(customResponse(200, user));
